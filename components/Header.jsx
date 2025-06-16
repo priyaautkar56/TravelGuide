c96515550edaf29bdf6934f8c41e648122c3034f
priyaautkar56 <priya.autkar@yahoo.com>
2025-06-16 17:32:19 -0400

    Initial commit

---
+++ components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header>
+      <img src="..\src\assets\seven.png" alt="bunny-icon" />
+      <h1>Travel Guide World</h1>
+    </header>
+  );
+}
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import Entry from "../components/Entry";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Entry />
+      <Entry />
+      <Entry />
+    </>
+  );
+}
