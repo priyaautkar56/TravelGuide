f00919eab0657a4b0cb6cbdddc815f374d2e4e39
priyaautkar56 <priya.autkar@yahoo.com>
2025-06-20 10:50:35 -0400

    Final touch

--- components/Entry.jsx
+++ components/Entry.jsx
@@ -3,17 +3,17 @@ export default function Entry(props) {
   return (
     <article className="journal-entry">
       <div className="main-image-container">
-        <img className="main-image" src={props.mPicture} />
+        <img className="main-image" src={props.img.src} alt={props.img.alt} />
       </div>
       <div className="info-container">
         <img className="marker" src="..\src\assets\eight.jpg" />
         <span className="country">{props.country}</span>
-        <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">
+        <a href={props.googleMapsLink} target="_blank">
           View on google maps
         </a>
-        <h2 className="entry-title">{props.placeName}</h2>
-        <p className="trip-dates">{props.date}</p>
-        <p className="entry-text">{props.information}</p>
+        <h2 className="entry-title">{props.title}</h2>
+        <p className="trip-dates">{props.dates}</p>
+        <p className="entry-text">{props.text}</p>
       </div>
     </article>
   );
--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,25 @@
 import Header from "../components/Header";
 import Entry from "../components/Entry";
+import data from "../data";
 
 export default function App() {
+  const entryElements = data.map((entry) => {
+    return (
+      <Entry
+        key={entry.id}
+        img={entry.img}
+        title={entry.title}
+        country={entry.country}
+        googleMapsLink={entry.googleMapsLink}
+        dates={entry.dates}
+        text={entry.text}
+      />
+    );
+  });
   return (
     <>
       <Header />
-      <Entry
-        mPicture="..\src\assets\nine.jpg"
-        country="Japan"
-        placeName="Mount-Fuji"
-        date="12 Jan, 2021 - 24 Jan, 2021"
-        information="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
-          (12,380 feet). Mount Fuji is the single most popular tourist site in
-          Japan, for both Japanese and foreign tourists."
-      />
-      <Entry
-        mPicture="..\src\assets\six.jpg"
-        country="USA"
-        placeName="California"
-        date="12 Jan, 2021 - 24 Jan, 2021"
-        information="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
-          (12,380 feet). Mount Fuji is the single most popular tourist site in
-          Japan, for both Japanese and foreign tourists."
-      />
-      <Entry
-        mPicture="..\src\assets\two.jpg"
-        country="India"
-        placeName="Amravati"
-        date="28 Jan, 2024 - 20 June, 2025"
-        information="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
-          (12,380 feet). Mount Fuji is the single most popular tourist site in
-          Japan, for both Japanese and foreign tourists."
-      />
+      <main>{entryElements}</main>
     </>
   );
 }
